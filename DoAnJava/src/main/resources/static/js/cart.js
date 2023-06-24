e1502f3c14a0ea9973ce97b6529e3b72429132fa
manduong2k2 <manduong2k2@gmail.com>
2023-06-24 20:58:01 +0700

    update 24/06/2023

---
+++ DoAnJava/src/main/resources/static/js/cart.js
@@ -0,0 +1,6 @@
+var items=[];
+
+            function AddToCard(id,id2){
+                let val=parseInt(document.getElementById('itemNumber').textContent)+id+id2;
+                document.getElementById('itemNumber').textContent=val;
+}
